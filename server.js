df339ad22b91ce8c2e4a3c8d8ea6dbace94eb14e
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2021-12-29 18:31:52 -0500

    WIP

--- server.js
+++ server.js
@@ -3,4 +3,11 @@ const app = express();
 // Now we just need to use one method to make our server listen. We're going to chain the listen() method onto our server to do it. To do that, add the following code to the end of server.js:
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
+
   });
+
+  app.get('/api/animals', (req, res) => {
+  res.json(animals);
+});
+
+  const { animals } = require('./data/animals');
